44602b733350d0a42f9bba4aac3da8ad9401aae7
Uttam Kumar <in.uttamkumar@gmail.com>
2018-04-01 13:33:54 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
 # Download IMDB datasets from here - https://datasets.imdbws.com/
-##
+# Documentation  - https://datasets.imdbws.com/
 import os
 import pandas as pd
 import difflib
@@ -13,14 +13,16 @@ movie_file_path = "C:\\NEO\\Movies"  # Path where movies are stored
 
 # Garbage words which are present in the movie Names
 replace_words = ["720p", "Bluray", "\.", "x264", "YTS AG", "YIFY", "www UsaBit com", "\[", "\]", "_", "anoXmous",
-                 "BrRip","sujaidr", "-", "\(", "\)", "Ganool", "TMRG", "1080p", "480p","10th Anniversary Edition" ]
+                 "BrRip","sujaidr", "-", "\(", "\)", "Ganool", "TMRG", "1080p", "480p","10th Anniversary Edition","DVDScr"
+                "XVID","AC3","EtMovies"]
 
 all_extension_list = []
 all_file_list_with_path = []
 video_file_list_with_path = []
 name_of_movie_list = []
 imdb_matched_movie_list = []
-
+year_of_movie_list = []
+name_of_movie_folder_list = [] 
 # Extracting the list of video files for which IMDB information would be extracted
 
 for roots, directories, filename in os.walk(movie_file_path):
@@ -34,12 +36,28 @@ unq_ext = set(all_extension_list)
 # print(unq_ext)
 
 for files in all_file_list_with_path:
-    if os.stat(files).st_size > 524288000:  # 500 MB , filtering files which are greater than 500 MB in size
+    if os.stat(files).st_size > 104857600:  # 00 MB , filtering files which are greater than 500 MB in size
         video_file_list_with_path.append(files)
 
+# Searching for 4 digits to extract YEAR
 for x in range(len(video_file_list_with_path)):
     name_of_movie_list.append(video_file_list_with_path[x].split("\\")[-1])
+    name_of_movie_folder_list.append(video_file_list_with_path[x].split("\\")[-2])
+    year = re.findall(r'\d\d\d\d',''.join(re.findall(r'\(\d\d\d\d\)',video_file_list_with_path[x].split("\\")[-2])))
+    if not year:
+        year = re.findall(r'\d\d\d\d',video_file_list_with_path[x].split("\\")[-2])
+    if not year:
+        year = re.findall(r'\d\d\d\d',''.join(re.findall(r'\(\d\d\d\d\)',video_file_list_with_path[x].split("\\")[-1])))
+    if not year:    
+        year = re.findall(r'\d\d\d\d',video_file_list_with_path[x].split("\\")[-1])
+    year = ''.join(year)    
+    year_of_movie_list.append(year)
 
+for x in range(len(name_of_movie_list)):
+    print(name_of_movie_folder_list[x],"-----",name_of_movie_list[x],"-----",year_of_movie_list[x])
+
+print("___________________________________________________________________________________________")
+print("___________________________________________________________________________________________")
 # Creating Pandas Data frame from the list of movie names
 df_movie = pd.DataFrame({"primaryTitle": name_of_movie_list})
 # Cleaning up the movie Names - removing extension from the name
@@ -50,43 +68,66 @@ for i in range(len(replace_words)):
     df_movie['MovieName'] = df_movie['MovieName'].apply(lambda x: re.sub(replace_words[i], " ", x, flags=re.IGNORECASE))
     df_movie['MovieName'] = df_movie['MovieName'].str.strip()
 
-# Searching for 4 digits to extract YEAR
-df_movie['year'] = df_movie['MovieName'].str.findall('\d{4}')
+
+df_movie['year'] = pd.Series(year_of_movie_list)
+df_movie['complete_path'] = pd.Series(video_file_list_with_path)
 # Removing year from the name
-df_movie['MovieName'] = df_movie['MovieName'].str.replace(r'\d{4}', "")
+for i in range(len(df_movie)):
+    df_movie.loc[i]['MovieName'] = df_movie.loc[i]['MovieName'].replace(str(df_movie.loc[i]['year']), "")
 # Removing Garbage words which represents size - 900MB etc
 df_movie['MovieName'] = df_movie['MovieName'].str.replace(r'\d+MB', "")
 # Reducing multiple spaces to one
 df_movie['MovieName'] = df_movie['MovieName'].str.replace(r' +', " ")
 df_movie['MovieName'] = df_movie['MovieName'].str.strip()
 
-print(df_movie)
 ################################################
 
 # Reading IMDB datasets in Pandas Data frame
-df_tbasics = pd.read_csv(imdb_data_files_tbasics, sep="\t", encoding="utf-8", dtype="unicode")
-df_tratings = pd.read_csv(imdb_data_files_tratings, sep="\t", encoding="utf-8", dtype="unicode")
+# df_tbasics = pd.read_csv(imdb_data_files_tbasics, sep="\t", encoding="utf-8", dtype="unicode")
+# df_tratings = pd.read_csv(imdb_data_files_tratings, sep="\t", encoding="utf-8", dtype="unicode")
 
 # Merging Title info and ratings into one Data frame
 df_ratings_basics = pd.merge(df_tratings, df_tbasics, on=['tconst'], how='inner')
 df_ratings_basics['runtimeMinutes'] = df_ratings_basics['runtimeMinutes'].str.replace(r'\\N',"0")
-
+imdb_matched_movie_list = []
 for i in range(len(df_movie)):
     if len(df_movie.loc[i]['year']) == 0:
         temp = difflib.get_close_matches(df_movie.loc[i]['MovieName'], df_ratings_basics['primaryTitle'], 1, cutoff=0.6)
         imdb_matched_movie_list.append(temp)
+#         print("year missing")
     else:
-        for j in range(len(df_movie.loc[i]['year'])):
-            df_ratings_basics_filtered = df_ratings_basics[(df_ratings_basics['startYear'] == df_movie['year'].loc[i][j])
-                                                    & (pd.to_numeric(df_ratings_basics['runtimeMinutes']) > 60)]
+        try:
+            df_ratings_basics_filtered = df_ratings_basics[(df_ratings_basics['startYear'] == df_movie['year'].loc[i])]
+#                                                     & (pd.to_numeric(df_ratings_basics['runtimeMinutes']) > 60)]
             temp = difflib.get_close_matches(df_movie.loc[i]['MovieName'], df_ratings_basics_filtered['primaryTitle'], 1,
                                              cutoff=0.6)
-            print(df_movie.loc[i]['MovieName'], "------", temp, "-----", df_movie['year'].loc[i][j])
-            if len(temp) > 0:
-                imdb_matched_movie_list.append(temp)
-                continue
+            imdb_matched_movie_list.append(''.join(temp))
+            print(df_movie.loc[i]['MovieName'], "------", temp, "-----", df_movie['year'].loc[i])
+        except ValueError:
+            print("Character values found in RuntimeMinutes")
 
-df_movie['imdb_matched_name'] = pd.DataFrame({'imdb_matched_name' : imdb_matched_movie_list})
+print("___________________________________________________________________________________________")
+print("___________________________________________________________________________________________")
+
+df_movie['imdb_matched_name'] = pd.Series(imdb_matched_movie_list)
+
+print(df_movie)
+
+print("___________________________________________________________________________________________")
+print("___________________________________________________________________________________________")
 
 df_movie_imdb = pd.merge(df_movie , df_ratings_basics, left_on = ['imdb_matched_name'], right_on = ['primaryTitle'],how='inner')
 
+pd.set_option('display.max_colwidth', -1)
+# pd.set_option('display.max_colwidth', -1)
+# # pd.set_option('display.height', 1000)
+# pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_columns', 10)
+# pd.set_option('display.width', 5000)
+# pd.set_option('display.expand_frame_repr', False)
+
+Movie_or_Serial = 'movie'
+delim = "___"
+df_movie_new = pd.merge(df_movie,df_ratings_basics[df_ratings_basics['titleType']==Movie_or_Serial],left_on=['imdb_matched_name','year'],right_on = ['primaryTitle','startYear'],how='inner')
+df_movie_new['to_rename'] = df_movie_new['imdb_matched_name'] + delim + df_movie_new['year'] + delim + df_movie_new['averageRating'] + delim + df_movie_new['runtimeMinutes'] + delim + df_movie_new['genres']
+print(df_movie_new)
