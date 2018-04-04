d695940c9a2fbae37b86d439ab2d5ed321ada83f
Uttam Kumar <in.uttamkumar@gmail.com>
2018-04-04 20:19:56 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -17,8 +17,8 @@ movie_file_path = "C:\\NEO\\Movies"  # Path where movies are stored
 
 # Garbage words which are present in the movie Names
 replace_words = ["720p", "Bluray", "\.", "x264", "YTS AG", "YIFY", "www UsaBit com", "\[", "\]", "_", "anoXmous",
-                 "BrRip", "sujaidr", "-", "\(", "\)", "Ganool", "TMRG", "1080p", "480p", "10th Anniversary Edition", "DVDScr"
-                 "XVID", "AC3", "EtMovies", "YTS"]
+                 "BrRip","sujaidr", "-", "\(", "\)", "Ganool", "TMRG", "1080p", "480p","10th Anniversary Edition","DVDScr"
+                "XVID","AC3","EtMovies","YTS"]
 Movie_or_Serial = 'movie'
 delim = "___"
 
@@ -28,7 +28,7 @@ video_file_list_with_path = []
 name_of_movie_list = []
 imdb_matched_movie_list = []
 year_of_movie_list = []
-name_of_movie_folder_list = []
+name_of_movie_folder_list = [] 
 
 # Extracting the list of video files for which IMDB information would be extracted
 for roots, directories, filename in os.walk(movie_file_path):
@@ -47,34 +47,34 @@ for files in all_file_list_with_path:
 
 # Searching for 4 digits to extract YEAR
 for x in range(len(video_file_list_with_path)):
-    with open(os.path.join(movie_file_path, "all_folder_names.txt"), 'a') as fh:
-        fh.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M') + " -- " + video_file_list_with_path[x] + "\n")
-        if x == len(video_file_list_with_path) - 1:
+    with open(os.path.join(movie_file_path,"all_folder_names.txt"),'a') as fh:
+        fh.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M') + " -- "  + video_file_list_with_path[x] + "\n")
+        if x == len(video_file_list_with_path)-1:
             fh.write("_________________________________________________________________________________________________________________" + "\n")
     name_of_movie_list.append(video_file_list_with_path[x].split("\\")[-1])
     name_of_movie_folder_list.append(video_file_list_with_path[x].split("\\")[-2])
-    year = re.findall(r'\d\d\d\d', ''.join(re.findall(r'\(\d\d\d\d\)', video_file_list_with_path[x].split("\\")[-2])))
+    year = re.findall(r'\d\d\d\d',''.join(re.findall(r'\(\d\d\d\d\)',video_file_list_with_path[x].split("\\")[-2])))
     if not year:
-        year = re.findall(r'\d\d\d\d', video_file_list_with_path[x].split("\\")[-2])
+        year = re.findall(r'\d\d\d\d',video_file_list_with_path[x].split("\\")[-2])
     if not year:
-        year = re.findall(r'\d\d\d\d', ''.join(re.findall(r'\(\d\d\d\d\)', video_file_list_with_path[x].split("\\")[-1])))
-    if not year:
-        year = re.findall(r'\d\d\d\d', video_file_list_with_path[x].split("\\")[-1])
-    year = ''.join(year)
+        year = re.findall(r'\d\d\d\d',''.join(re.findall(r'\(\d\d\d\d\)',video_file_list_with_path[x].split("\\")[-1])))
+    if not year:    
+        year = re.findall(r'\d\d\d\d',video_file_list_with_path[x].split("\\")[-1])
+    year = ''.join(year)    
     year_of_movie_list.append(year)
-    if movie_file_path == video_file_list_with_path[x].rsplit("\\", 1)[0]:
-        print("This movie is not in its own folder -- ", video_file_list_with_path[x].split("\\")[-1])
-        if not os.path.isdir(os.path.join(movie_file_path, video_file_list_with_path[x].split("\\")[-1].rsplit(".", 1)[0])):
-            os.makedirs(os.path.join(movie_file_path, video_file_list_with_path[x].split("\\")[-1].rsplit(".", 1)[0]))
-        shutil.move(video_file_list_with_path[x], os.path.join(movie_file_path, video_file_list_with_path[x].split("\\")[-1].rsplit(".", 1)[0]))
-    with open(os.path.join(video_file_list_with_path[x].rsplit("\\", 1)[0], "Previous_folder_names.txt"), "a") as fh:
-        fh.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M') + " -- Previous Folder Name - " + "'" + video_file_list_with_path[x].split("\\")[-2] + "'" + "\n")
-
+    if movie_file_path == video_file_list_with_path[x].rsplit("\\",1)[0]:
+        print("This movie is not in its own folder -- ",video_file_list_with_path[x].split("\\")[-1])
+        if not os.path.isdir(os.path.join(movie_file_path,video_file_list_with_path[x].split("\\")[-1].rsplit(".",1)[0])):
+            os.makedirs(os.path.join(movie_file_path,video_file_list_with_path[x].split("\\")[-1].rsplit(".",1)[0]))
+        shutil.move(video_file_list_with_path[x],os.path.join(movie_file_path,video_file_list_with_path[x].split("\\")[-1].rsplit(".",1)[0]))
+    with open(os.path.join(video_file_list_with_path[x].rsplit("\\",1)[0],"Previous_folder_names.txt"),"a") as fh:
+              fh.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M') + " -- Previous Folder Name - " + "'" + video_file_list_with_path[x].split("\\")[-2] + "'" + "\n")              
+    
 
 for x in range(len(name_of_movie_list)):
-    print(name_of_movie_folder_list[x], "-----", name_of_movie_list[x], "-----", year_of_movie_list[x])
+    print(name_of_movie_folder_list[x],"-----",name_of_movie_list[x],"-----",year_of_movie_list[x])
 
-print("___________________________________________________________________________________________")
+print("__________________________________1_________________________________________________________")
 print("___________________________________________________________________________________________")
 # Creating Pandas Data frame from the list of movie names
 df_movie = pd.DataFrame({"primaryTitle": name_of_movie_list})
@@ -106,7 +106,7 @@ df_tratings = pd.read_csv(imdb_data_files_tratings, sep="\t", encoding="utf-8", 
 
 # Merging Title info and ratings into one Data frame
 df_ratings_basics = pd.merge(df_tratings, df_tbasics, on=['tconst'], how='inner')
-df_ratings_basics['runtimeMinutes'] = df_ratings_basics['runtimeMinutes'].str.replace(r'\\N', "0")
+df_ratings_basics['runtimeMinutes'] = df_ratings_basics['runtimeMinutes'].str.replace(r'\\N',"0")
 imdb_matched_movie_list = []
 for i in range(len(df_movie)):
     if len(df_movie.loc[i]['year']) == 0:
@@ -124,17 +124,17 @@ for i in range(len(df_movie)):
         except ValueError:
             print("Character values found in RuntimeMinutes")
 
-print("___________________________________________________________________________________________")
+print("____________________________________2_______________________________________________________")
 print("___________________________________________________________________________________________")
 
 df_movie['imdb_matched_name'] = pd.Series(imdb_matched_movie_list)
 
 print(df_movie)
 
-print("___________________________________________________________________________________________")
+print("___________________________________3________________________________________________________")
 print("___________________________________________________________________________________________")
 
-df_movie_imdb = pd.merge(df_movie, df_ratings_basics, left_on=['imdb_matched_name'], right_on=['primaryTitle'], how='inner')
+df_movie_imdb = pd.merge(df_movie , df_ratings_basics, left_on = ['imdb_matched_name'], right_on = ['primaryTitle'],how='inner')
 
 # pd.set_option('display.max_colwidth', -1)
 # # pd.set_option('display.height', 1000)
@@ -143,15 +143,20 @@ df_movie_imdb = pd.merge(df_movie, df_ratings_basics, left_on=['imdb_matched_nam
 # pd.set_option('display.width', 5000)
 # pd.set_option('display.expand_frame_repr', False)
 
-df_movie_new = pd.merge(df_movie, df_ratings_basics[df_ratings_basics['titleType'] == Movie_or_Serial], left_on=['imdb_matched_name', 'year'], right_on=['primaryTitle', 'startYear'], how='inner')
+
+df_movie_new = pd.merge(df_movie,df_ratings_basics[df_ratings_basics['titleType']==Movie_or_Serial],left_on=['imdb_matched_name','year'],right_on = ['primaryTitle','startYear'],how='inner')
 df_movie_new['to_rename'] = df_movie_new['imdb_matched_name'] + delim + df_movie_new['year'] + delim + df_movie_new['averageRating'] + delim + df_movie_new['runtimeMinutes'] + delim + df_movie_new['genres']
 print(df_movie_new)
 
-print("___________________________________________________________________________________________")
-print("___________________________________________________________________________________________")
 
-df_movie_new.index = df['complete_path'].str.len()
+df_movie_new.index = df_movie_new['complete_path'].str.len()
 df_movie_new = df_movie_new.sort_index(ascending=False).reset_index(drop=True)
 
+print("__________________________________4_________________________________________________________")
+print("___________________________________________________________________________________________")
+
 for x in range(len(df_movie_new)):
-    os.rename(video_file_list_with_path[x].rsplit("\\", 1)[0], os.path.join(video_file_list_with_path[x].rsplit("\\", 1)[0], df_movie_new.loc[x]['to_rename']))
+    df_movie_new.loc[x]['to_rename'] = df_movie_new.loc[x]['to_rename'].replace(":","")
+    os.rename(df_movie_new.loc[x]['complete_path'].rsplit("\\",1)[0],os.path.join(df_movie_new.loc[x]['complete_path'].rsplit("\\",2)[0],df_movie_new.loc[x]['to_rename']))
+
+                 
